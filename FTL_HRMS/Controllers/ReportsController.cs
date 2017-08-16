70c0617ffd3e601b4537eacc4fb903584a67cddb
yeasinmahi <yeasinmahi72@yahoo.com>
2017-08-16 14:56:40 +0600

    adding Utility and report folder

---
+++ FTL_HRMS/Controllers/ReportsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using CrystalDecisions.Web;
+using FTL_HRMS.Models;
+
+namespace FTL_HRMS.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Reports"), "test.rpt"));
+            rd.SetDatabaseLogon("sa", "sa2009", ".\\SQLEXPRESS", "FTL_HRMS");
+
+            IEnumerable<Employee> employees;
+            using (HRMSDbContext dc = new HRMSDbContext())
+            {
+                employees = dc.Employee.Select(x => new Employee()
+                {
+                    Code = x.Code,
+                    Name = x.Name,
+                    FathersName = x.FathersName
+                }).AsEnumerable();
+                rd.SetDataSource(employees);
+            }
+
+            
+            
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "EverestList.pdf");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            //ReportDocument Report = new ReportDocument();
+            //Report.Load(Server.MapPath("~/Reports/test.rpt"));
+            //Report.SetDatabaseLogon("sa", "sa2009", ".\\SQLEXPRESS", "FTL_HRMS");
+
+
+
+            //ExportOptions CrExportOptions;
+            //DiskFileDestinationOptions CrDiskFileDestinationOptions = new DiskFileDestinationOptions();
+            //PdfRtfWordFormatOptions CrFormatTypeOptions = new PdfRtfWordFormatOptions();
+            //CrDiskFileDestinationOptions.DiskFileName = "f:\\test.pdf";
+            //CrExportOptions = Report.ExportOptions;
+            //{
+            //    CrExportOptions.ExportDestinationType = ExportDestinationType.DiskFile;
+            //    CrExportOptions.ExportFormatType = ExportFormatType.PortableDocFormat;
+            //    CrExportOptions.DestinationOptions = CrDiskFileDestinationOptions;
+            //    CrExportOptions.FormatOptions = CrFormatTypeOptions;
+            //}
+            //Report.Export();
+            return View();
+        }
+
+        private ActionResult GenerateJobCardReport()
+        {
+            List<Employee> allEverest;
+            using (HRMSDbContext dc = new HRMSDbContext())
+            {
+                allEverest = dc.Employee.ToList();
+            }
+
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Reports"), "test.rpt"));
+            rd.SetDatabaseLogon("sa", "sa2009", ".\\SQLEXPRESS", "FTL_HRMS");
+            rd.SetDataSource(allEverest);
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "EverestList.pdf");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ FTL_HRMS/Utility/GenericData.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Reflection;
+using FTL_HRMS.Models;
+
+namespace FTL_HRMS.Utility
+{
+    public class GenericData
+    {
+        readonly object _obj = new object();
+        
+        private static GenericData _instance;
+        readonly HRMSDbContext _dbContext = new HRMSDbContext();
+
+        private GenericData() { }
+      
+        public static GenericData GetInstance()
+        {
+            return _instance ?? (_instance = new GenericData());
+        }
+
+        
+        public List<T> ListAll<T>() where T : class
+        {
+            lock (_obj)
+            {
+               List<T> genericList= _dbContext.Set(typeof(T)).Cast<T>().AsNoTracking<T>().ToList();
+                return genericList;
+            }
+
+        }
+        public T ListAllById<T>(int id) where T : class
+        {
+            lock (_obj)
+            {
+                return _dbContext.Set(typeof(T)).Cast<T>().Find(id);
+            }
+            
+
+        }
+        public string Insert<T>(object o)
+        {
+            Type type = typeof (T);
+            lock (_obj)
+            {
+                try
+                {
+                    _dbContext.Set(type).Add(o);
+                    _dbContext.SaveChanges();
+                    return DbUtility.GetStatusMessage(DbUtility.Status.Success);
+                }
+                catch (DbUpdateException dbEx)
+                {
+                    return "Db Update Failed";
+                }
+                catch (Exception ex)
+                {
+                    return DbUtility.GetStatusMessage(DbUtility.Status.Fail);
+                }
+            }
+        }
+
+        public string Update<T>(int id, string prop, string value) where T : class
+        {
+            lock (_obj)
+            {
+                T bank = _dbContext.Set(typeof (T)).Cast<T>().Find(id);
+                PropertyInfo propertyInfo = DbUtility.GetPropertyInfo(bank, prop);
+                if (propertyInfo != null)
+                {
+                    if (DbUtility.SetValue(bank, propertyInfo, value))
+                    {
+                        try
+                        {
+                            _dbContext.SaveChanges();
+                            return DbUtility.GetStatusMessage(DbUtility.Status.Success);
+                        }
+                        catch (Exception)
+                        {
+                            return DbUtility.GetStatusMessage(DbUtility.Status.Fail);
+                        }
+                    }
+                    return DbUtility.GetStatusMessage(DbUtility.Status.Error);
+                }
+                return DbUtility.GetStatusMessage(DbUtility.Status.Null);
+            }
+        }
+
+        public string Delete<T>(int id) where T : class
+        {
+            
+            lock (_obj)
+            {
+                using (HRMSDbContext db = new HRMSDbContext())
+                {
+                    try
+                    {
+                        Type type = typeof(T);
+                        T bank = db.Set(typeof(T)).Cast<T>().Find(id);
+                        if (bank != null)
+                        {
+                            db.Set(type).Attach(bank);
+                            db.Set(type).Remove(bank);
+                            db.SaveChanges();
+                            return DbUtility.GetStatusMessage(DbUtility.Status.Success);
+                        }
+                        return DbUtility.GetStatusMessage(DbUtility.Status.Null);
+                    }
+                    catch (Exception)
+                    {
+                        return DbUtility.GetStatusMessage(DbUtility.Status.Fail);
+                    }
+                }
+                
+            }
+        }
+    }
+}
