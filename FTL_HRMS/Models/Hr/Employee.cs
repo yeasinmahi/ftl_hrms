bd3d3732b659991ceb4d6140aa4b049d683e8e26
aniksaha <pappubd007@gmail.com>
2017-09-10 16:43:07 +0600

    adding parmanent date to model

--- FTL_HRMS/Models/Hr/Employee.cs
+++ FTL_HRMS/Models/Hr/Employee.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data;
 
 namespace FTL_HRMS.Models.Hr
 {
@@ -19,8 +18,8 @@ namespace FTL_HRMS.Models.Hr
         [Key]
         public int Sl { get; set; }
         [MaxLength(15)]
-        [Required(ErrorMessage = "Code cannot be empty")]
-        [Index(IsUnique = true)]
+        [Required(ErrorMessage ="Code can not be empty")]
+        [Index(IsUnique =true)]
         public string Code { get; set; }
 
         [Required(ErrorMessage = "Name cannot be empty")]
