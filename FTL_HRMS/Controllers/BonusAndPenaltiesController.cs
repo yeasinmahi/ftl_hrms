d8f51b2a942cc41450d0f19899904f8e08633ec3
aniksaha <pappubd007@gmail.com>
2017-09-17 13:22:13 +0600

    BonusAndPenalties

---
+++ FTL_HRMS/Controllers/BonusAndPenaltiesController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FTL_HRMS.Models;
+using FTL_HRMS.Models.Payroll;
+using FTL_HRMS.Utility;
+
+namespace FTL_HRMS.Controllers
+{
+    public class BonusAndPenaltiesController : Controller
+    {
+        private HRMSDbContext _db = new HRMSDbContext();
+
+        // GET: BonusAndPenalties
+        public ActionResult Index()
+        {
+            var bonusAndPenalty = _db.BonusAndPenalty.Include(b => b.CreateEmployee).Include(b => b.Employee).Include(b => b.UpdateEmployee);
+            return View(bonusAndPenalty.ToList());
+        }
+
+        // GET: BonusAndPenalties/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BonusAndPenalty bonusAndPenalty = _db.BonusAndPenalty.Find(id);
+            if (bonusAndPenalty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bonusAndPenalty);
+        }
+
+        // GET: BonusAndPenalties/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeId = new SelectList(_db.Employee, "Sl", "Code");
+            return View();
+        }
+
+        // POST: BonusAndPenalties/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Sl,EmployeeId,Date,Type,Amount,Remarks,CreatedBy,CreateDate,UpdatedBy,UpdateDate")] BonusAndPenalty bonusAndPenalty)
+        {
+            if (bonusAndPenalty.Type != "")
+            {
+                bonusAndPenalty.EmployeeId = bonusAndPenalty.EmployeeId;
+                string userName = User.Identity.Name;
+                int userId = DbUtility.GetUserId(_db, userName);
+                bonusAndPenalty.CreatedBy = userId;
+                bonusAndPenalty.CreateDate = DateTime.Now;
+                _db.BonusAndPenalty.Add(bonusAndPenalty);
+                _db.SaveChanges();
+                TempData["SuccessMsg"] = "Added Successfully !!";
+                return RedirectToAction("Create");
+            }
+            TempData["WarningMsg"] = "Something went wrong !!";
+            ViewBag.EmployeeId = new SelectList(_db.Employee, "Sl", "Code");
+            return View(bonusAndPenalty);
+
+        }
+
+        // GET: BonusAndPenalties/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BonusAndPenalty bonusAndPenalty = _db.BonusAndPenalty.Find(id);
+            if (bonusAndPenalty == null)
+            {
+                return HttpNotFound();
+            }
+            
+            ViewBag.EmployeeId = new SelectList(_db.Employee, "Sl", "Code", bonusAndPenalty.EmployeeId);
+            
+            return View(bonusAndPenalty);
+        }
+
+        // POST: BonusAndPenalties/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Sl,EmployeeId,Date,Type,Amount,Remarks,CreatedBy,CreateDate,UpdatedBy,UpdateDate")] BonusAndPenalty bonusAndPenalty)
+        {
+            if (ModelState.IsValid)
+            {
+                string userName = User.Identity.Name;
+                int UserId = DbUtility.GetUserId(_db, userName);
+                bonusAndPenalty.UpdatedBy = UserId;
+                bonusAndPenalty.UpdateDate = DateTime.Now;
+                _db.Entry(bonusAndPenalty).State = EntityState.Modified;
+                _db.SaveChanges();
+                TempData["SuccessMsg"] = "Added Successfully !!";
+                return RedirectToAction("Edit");
+            }
+            TempData["WarningMsg"] = "Something went wrong !!";
+            ViewBag.EmployeeId = new SelectList(_db.Employee, "Sl", "Code", bonusAndPenalty.EmployeeId);
+            return View(bonusAndPenalty);
+        }
+
+        // GET: BonusAndPenalties/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BonusAndPenalty bonusAndPenalty = _db.BonusAndPenalty.Find(id);
+            if (bonusAndPenalty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bonusAndPenalty);
+        }
+
+        // POST: BonusAndPenalties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BonusAndPenalty bonusAndPenalty = _db.BonusAndPenalty.Find(id);
+            _db.BonusAndPenalty.Remove(bonusAndPenalty);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
