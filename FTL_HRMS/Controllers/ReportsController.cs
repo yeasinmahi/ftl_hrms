dae6ec9ed50b2a0900c5ed09c1de090546e65ffa
aniksaha <pappubd007@gmail.com>
2017-10-05 18:11:58 +0600

    attendnc report

--- FTL_HRMS/Controllers/ReportsController.cs
+++ FTL_HRMS/Controllers/ReportsController.cs
@@ -347,7 +347,7 @@ namespace FTL_HRMS.Controllers
             int employeeId = Convert.ToInt32(Request["EmployeeId"]);
             DateTime fromDate = Convert.ToDateTime(Request["fromDate"]);
             DateTime toDate = Convert.ToDateTime(Request["toDate"]);
-            return RedirectToAction("PrintReport", "Reports", new { sourceName = "AttandanceByDateRangeReport", fileName = "Attandance By Date Range Report", selectedFormula = "{tbl_MonthlyAttendance.FromDate}>=Date (" + fromDate.ToString("yyyy,MM,dd") + ") and {tbl_MonthlyAttendance.ToDate}<= Date (" + toDate.ToString("yyyy,MM,dd") + " ) and {tbl_Employee.Sl}= "+employeeId+"" });
+            return RedirectToAction("PrintReport", "Reports", new { sourceName = "AttandanceByDateRangeReport", fileName = "Attandance By Date Range Report", selectedFormula = "{tbl_MonthlyAttendance.Date}>=Date (" + fromDate.ToString("yyyy,MM,dd") + ") and {tbl_MonthlyAttendance.Date}<= Date (" + toDate.ToString("yyyy,MM,dd") + " ) and {tbl_Employee.Sl}= "+employeeId+"" });
         }
         #endregion
 
