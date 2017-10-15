022259047f9ce8068a6dbb8f73220146c516a113
yeasinmahi <yeasinmahi72@yahoo.com>
2017-10-15 16:52:21 +0600

    adding new deviceContext

---
+++ FTL_HRMS/DAL/DeviceDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using FTL_HRMS.Models;
+using FTL_HRMS.Models.Payroll;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FTL_HRMS.DAL
+{
+    public class DeviceDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DeviceDbContext():base("DeviceDbContext")
+        {
+            Database.SetInitializer<DeviceDbContext>(null);
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        public DbSet<DeviceAttendance> DeviceAttendance { get; set; }
+        
+    }
+}
