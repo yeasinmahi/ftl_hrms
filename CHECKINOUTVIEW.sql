cc2f5faa74d0e90825a38fd6c7011a345fdcfc77
yeasinmahi <yeasinmahi72@yahoo.com>
2017-10-26 18:10:17 +0600

    change CHECKINOUTVIEW

--- CHECKINOUTVIEW.sql
+++ CHECKINOUTVIEW.sql
@@ -2,5 +2,5 @@ go
 ALTER TABLE CHECKINOUT
 ADD IsCalculate bit DEFAULT 0
 go
-create view CHECKINOUTVIEW as 
-select u.Badgenumber,c.CHECKTIME,IsCalculate from userinfo as u join CHECKINOUT as c on c.USERID=u.USERID
+create view [dbo].[CHECKINOUTVIEW] as 
+select u.USERID, u.Badgenumber as EmployeeCode,c.CHECKTIME,IsCalculate from userinfo as u join CHECKINOUT as c on c.USERID=u.USERID
